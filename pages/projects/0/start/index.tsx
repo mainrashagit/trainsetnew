a28f4a8c17f1f28e3b78876611183fcc2ad6a8a8
ripinsurreality <ripinsurreality@gmail.com>
2021-07-23 16:59:54 +0300

    add project start page; fix logo

---
+++ pages/projects/0/start/index.tsx
@@ -0,0 +1,13 @@
+import styles from './index.module.sass'
+
+interface Props {}
+
+const index: React.FC<Props> = ({}) => {
+    return (
+        <div className={styles.container}>
+        
+        </div>
+    )
+}
+
+export default index
